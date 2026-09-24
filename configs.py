96440f606f2b18e316aab2c88346a603e4f809e6
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-24 22:56:58 +0500

    I created this file to store static settings, for example paths, epochs or learning rates and so on.

---
+++ configs.py
@@ -0,0 +1,10 @@
+"""
+configs.py
+~~~~~~~~~~~
+
+This file will store all static settings of the project. 
+As an example, I can say that - here I will store epoch, 
+learning rate, paths and so on.
+"""
+
+...
