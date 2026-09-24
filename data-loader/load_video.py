d9fa63a6de4483e03c0e26a4aca29ec360567dc6
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-24 23:02:52 +0500

    I created this file to load vide data and then return make it a frames.

---
+++ data-loader/load_video.py
@@ -0,0 +1,15 @@
+"""
+load_video.py
+~~~~~~~~~~~~~~
+
+This file will help me to load the vide data to augmentation process. 
+Basically, it will read  video data and  make frames. Then all frames
+sent to  augmentations.py file to process. I will also use methods.py 
+file to  load some important  methods. All methods  written inside of 
+methods, but this file will use combinations of methods that can give 
+me desired output.
+"""
+
+class LoadVideo:
+    def __init__(self):
+        ...
