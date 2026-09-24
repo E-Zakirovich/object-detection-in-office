a440ce663fac65c69faf119682d2df40a3189511
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-24 22:56:18 +0500

    I created main.py file in order to run the whole project.

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+main.py
+~~~~~~~~
+
+With this file, I can be able to run the whole project at once.
+Basically, it connected to data loader, train and test fodlers
+at the same time.
+"""
+
+...
