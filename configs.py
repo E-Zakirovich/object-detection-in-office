7629a65d4852d204c5ce3ddf5facfea0a0321837
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-25 00:52:57 +0500

    I added new path settigs

--- configs.py
+++ configs.py
@@ -7,4 +7,8 @@ As an example, I can say that - here I will store epoch,
 learning rate, paths and so on.
 """
 
-...
+# path settings
+video_data = "./data/raw/video/day_2.avi"
+ground_truth = "./data/raw/ground-truth"
+frame_names = "./data/raw/frame-names"
+path_to_processed_data = "./data/processed"
