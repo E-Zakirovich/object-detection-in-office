9ca5b046ad696fa7d3f0c5d0426e2beec070e7f5
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-24 23:01:58 +0500

    I created this file to load vide data to artificial intelligence model.

---
+++ data-loader/load.py
@@ -0,0 +1,13 @@
+"""
+load.py
+~~~~~~~~
+
+load.py  file is  foindation of my  project, basically  this 
+file is connected to other three files inside of data-loader
+folder. Also, this  folder will receive the data from ./data
+folder and sends it to YOLO.
+"""
+
+class Load:
+    def __init___(self):
+        ...
