52c33de65d7252ff28713c8cbef8cf85730f10d5
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-25 00:53:33 +0500

    fram maker method is working sucessfully

--- main.py
+++ main.py
@@ -7,4 +7,14 @@ Basically, it connected to data loader, train and test fodlers
 at the same time.
 """
 
-...
+from dataloader.load_video import LoadVideo
+import configs as get
+
+def main():
+    loader = LoadVideo(get.video_data, get.path_to_processed_data)
+    n = loader.make_frames()
+    print(f"Saved {n} frames")
+
+
+if __name__ == "__main__":
+    main()
