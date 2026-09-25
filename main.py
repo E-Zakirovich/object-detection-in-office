41b09a69a091060d99722c6d459995d371fefe5f
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-25 11:55:29 +0500

    calling frame maker method is optimized.

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ at the same time.
 """
 
 from dataloader.load_video import LoadVideo
+from dataloader.load import Load
 import configs as get
+import os
+
+# I am calling classess
+frame_maker = LoadVideo(get.video_data, get.path_to_processed_data)
+
 
 def main():
-    loader = LoadVideo(get.video_data, get.path_to_processed_data)
-    n = loader.make_frames()
-    print(f"Saved {n} frames")
+
+    # check, is there any data inside of processed folder, if yes go to pipeline, otherwise make frames and then go to data pipeline
+    total_files : int = os.listdir(get.path_to_processed_data)
+    if len(total_files) == 0:
+        # I am making frames from videos
+        print("I am making frames right now")
+        n = frame_maker.make_frames()
+        print(f"Saved {n} frames")
+
+    
+
+    
+
 
 
 if __name__ == "__main__":
