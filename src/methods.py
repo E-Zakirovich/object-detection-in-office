8c037cc6fc6093267855a46b1d6369484bd6dee2
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-27 01:18:04 +0500

    I need this folder in order to write method that used other classes.

---
+++ src/methods.py
@@ -0,0 +1,12 @@
+"""
+methods.py
+~~~~~~~~~~
+
+I need this file to write all methods that used inside of 
+other codebases. I just wanna  avoid to repeat write same
+process for different operations.
+"""
+
+class Methods:
+    def __init__(self):
+        ...
