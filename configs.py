9ded63bd6dcf3b42f006a033738db2c7b6ca4818
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-26 16:51:18 +0500

    path settings written inside.

---
+++ configs.py
@@ -0,0 +1,19 @@
+"""
+configs.pt
+~~~~~~~~~~
+
+I create this file in order  to store all settings related to whole project. 
+Thsi file [configs.py] is used all files,  for training, calculation of loss
+or image view part. Basically, this file will handle static settings part of
+the project.
+"""
+
+# path settings
+processed_train_images = "./data/processed/train/images"
+processed_train_labels = "./data/processed/train/labels"
+processed_validation_images = "./data/processed/validation/images"
+processed_validation_labels = "./data/processed/validation/labels"
+processed_test_images = "./data/processed/test/images/"
+processed_test_labels = "./data/processed/test/labels"
+ground_truth = "./data/ground_truth"
+video_data = "./data/video"
