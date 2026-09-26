429852a78722a44be556bb8b6c9d3fa3635f9229
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-27 01:36:17 +0500

    folder maker method is added

--- src/methods.py
+++ src/methods.py
@@ -7,6 +7,22 @@ other codebases. I just wanna  avoid to repeat write same
 process for different operations.
 """
 
+import os
+import configs as get
+
 class Methods:
     def __init__(self):
-        ...
+        self.name = "Office, employee detection"
+
+    # when I upload the code to github, data  folder will be  skipped because of .gitignore
+    # thats why, I made a method to make a folder for dataset. Just save time in the future
+    def make_folders(self):
+        # foder creation part of the project
+        os.makedirs(get.processed_train_images, exist_ok=True) # create folder for train images
+        os.makedirs(get.processed_train_labels, exist_ok=True) # create folder for train labels
+        os.makedirs(get.processed_validation_images, exist_ok=True) # create folder for validation images
+        os.makedirs(get.processed_validation_labels, exist_ok=True) # create folder for validation labels
+        os.makedirs(get.processed_test_images, exist_ok=True) # create folder for test images
+        os.makedirs(get.processed_test_labels, exist_ok=True) # create folder for test labels
+        os.makedirs(get.ground_truth, exist_ok=True) # create folder for groud truth
+        os.makedirs(get.video_data, exist_ok=True) # create folder for video data
